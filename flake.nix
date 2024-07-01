e265d601e10fd3e0744cde1487c132336b8fab3f
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2024-07-01 16:14:36 -0600

    add markdownlint

--- flake.nix
+++ flake.nix
@@ -5,16 +5,15 @@
 
   outputs = { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-    in
-    {
+      supportedSystems =
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f:
+        nixpkgs.lib.genAttrs supportedSystems
+        (system: f { pkgs = import nixpkgs { inherit system; }; });
+    in {
       devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [ quarto ];
-        };
+        default =
+          pkgs.mkShell { packages = with pkgs; [ quarto markdownlint-cli ]; };
       });
     };
 }
